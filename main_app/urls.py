ddec42f9c40f75452f7f7553d97396dacb7bd0ad
AlexChen208 <alex.chencs207@gmail.com>
2022-07-11 13:32:29 -0700

    creating routes to render templates/

---
+++ main_app/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+# the '.' represent that we are on the same file line, like a sibling file
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('travels/', views.travels_index, name='index'),
+]
--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+
+class Travel:  # Note that parens are optional if not inheriting from another class
+  def __init__(self, title, location, description, rating):
+    self.title = title
+    self.location = location
+    self.description = description
+    self.rating = rating
+
+travels = [
+  Travel('Chillin like a Villian', 'Disneyland', 'crowded as usual', 8),
+  Travel('GOOD GRUBBIN', 'Ave 26', 'Al pastor BOMB', 10),
+  Travel('Got the club going up', 'Terra Cotta', 'Dont remember what happend', 11)
+]
+
 
 # Create your views here.
+def home(request):
+    return HttpResponse('<h1>Hello</h1>')
+
+def about(request):
+    return render(request, 'about.html')
+
+def travels_index(request):
+    return render(request, 'travels/index.html', { 'travels': travels})
