efc59bc8f9cac0f3cd9b07442d8bb94f74411284
AlexChen208 <alex.chencs207@gmail.com>
2022-07-11 23:23:55 -0700

    creating detail page

--- main_app/views.py
+++ main_app/views.py
@@ -14,4 +14,6 @@ def travels_index(request):
     travels = Travel.objects.all()
     return render(request, 'travels/index.html', { 'travels': travels })
 
-
+def travels_detail(request, travel_id):
+    travel = Travel.objects.get(id=travel_id)
+    return render(request, 'travels/detail.html', { 'travel': travel })
