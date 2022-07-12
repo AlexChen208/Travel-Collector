f40b8f1b1c94e4a9b6115fc7b648fda365e6453c
AlexChen208 <alex.chencs207@gmail.com>
2022-07-12 11:29:16 -0700

    creating CBV for list page

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
-    path('travels/', views.travels_index, name='index'),
+    path('travels/', views.TravelList.as_view(), name='index'),
     path('travels/<int:travel_id>/', views.travels_detail, name='detail'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from . models import Travel
 
 
@@ -11,10 +11,19 @@ def home(request):
 def about(request):
     return render(request, 'about.html')
 
-def travels_index(request):
-    travels = Travel.objects.all()
-    return render(request, 'travels/index.html', { 'travels': travels })
+# def travels_index(request):
+#     travels = Travel.objects.all()
+#     return render(request, 'travels/index.html', { 'travels': travels })
 
-def travels_detail(request, travel_id):
-    travel = Travel.objects.get(id=travel_id)
-    return render(request, 'travels/detail.html', { 'travel': travel })
+# def travels_detail(request, travel_id):
+#     travel = Travel.objects.get(id=travel_id)
+#     return render(request, 'travels/detail.html', { 'travel': travel })
+
+class TravelList(ListView):
+    model = Travel
+    template_name = 'travels/index.html'
+
+# class TravelDetail(DetailView):
+#     model = Travel
+#     template_name = 'travels/detail.html'
+
