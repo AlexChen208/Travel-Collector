72271647c539d4d5663f300487dc09945f768413
AlexChen208 <alex.chencs207@gmail.com>
2022-07-11 23:18:28 -0700

    adding id path

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('travels/', views.travels_index, name='index'),
-    
+    path('travels/<int:travel_id>/', views.travels_detail, name='detail'),
 ]
